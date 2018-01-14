3506a512f2dc84b5387013a252459f260a2b1d46
Valerio Sillari <valerio.sillari@gmail.com>
2018-01-14 02:06:36 +0100

    [skip ci] update array

--- components/MapGoogle/_placesIdArrays.js
+++ components/MapGoogle/_placesIdArrays.js
@@ -17,38 +17,86 @@ const placeIdArray = [
   'ChIJyaJmgJ9PqEcRmFMyFfFqJGQ',
   // 009 - Vux
   'ChIJC-mZa6BPqEcRMXhPH5jFJyo',
-  // 010 - tier
+  // 010 - Böhmisches Dorf
+  'ChIJZYdI_KBPqEcRpHBUVSa86UQ',
+
+  // 011 - CocoLiebe
+  'ChIJy91K4KNPqEcRCAd6KD6oZx8',
+  // 012 - Berliner Berg Brauerei
+  'ChIJC8oyvqJPqEcRJiH6bTWiOos',
+  // 013 - Sameheads
+  'ChIJn5Np3qNPqEcRrRkTsDUHeEg',
+  // 014 - Klunkerkranich
+  'ChIJBfIPPbtPqEcRMnne4FZmMfk',
+  // 015 - K-Fetisch
+  'ChIJwbcY56VPqEcRWeH0D3fTHf0',
+  // 016 - Ä
+  'ChIJz61EdaZPqEcRZb1ZzQXwx_4',
+  // 017 - Vin Aqua Vin
+  'ChIJ06r-0LBPqEcR9tmvBjs5ab0',
+  // 018 - Damensalon
+  'ChIJp0sppLZPqEcRasG_5n9uw3A',
+  // 019 - tier
   'ChIJOSWgYaVPqEcR63RnoNfgsDg',
+  // 020 - Fuks Bar
+  'ChIJIzn297ZPqEcRgw6RoWPK3GU',
 
-  // ------------------------------------------
-  // more than 10
+  // 021 - Myxa Berlin
+  'ChIJn6jqirZPqEcRSTM-cJbVGAY',
+  // 022 - Roamers
+  'ChIJ_UQ2CbpPqEcRzhfVydN_PCs',
+  // 023 - Geist im Glas
+  'ChIJo99QRrZPqEcRXW-E2PeTJ4A',
+  // 024 - Cafe Jacques
+  'ChIJccDCX8pPqEcRkdhWnC8AyNI',
+  // 025 - Fuchsbau
+  'ChIJRSQRf8tPqEcRw5esshbdL8k',
+  // 026 - Molecule Men
+  'ChIJlRNaZ_5OqEcRom1mEqQnRcw',
+  // 027 - Insel der Jugend
+  'ChIJeZcqEh9PqEcRXsBoKa1ZkIk',
+  // 028 - Friedhof Alt-Stralau
+  'ChIJ_So_Jx5PqEcRoulQtLXbCkM',
+  // 029 - Arena Berlin
+  'ChIJC0xKsFVOqEcRhjEGUJP9Umg',
+  // 030 - Pony Saloon Berlin
+  'ChIJqwCqdclPqEcRYUkH0bnBeFk',
 
-  // // 011 - CocoLiebe
-  // 'ChIJy91K4KNPqEcRCAd6KD6oZx8',
-  // // 012 - Sameheads
-  // 'ChIJn5Np3qNPqEcRrRkTsDUHeEg',
-  // // 013 - Ä
-  // 'ChIJz61EdaZPqEcRZb1ZzQXwx_4',
-  // // 014 - Roamers
-  // 'ChIJ_UQ2CbpPqEcRzhfVydN_PCs',
-  // // 015 - Vin Aqua Vin
-  // 'ChIJ06r-0LBPqEcR9tmvBjs5ab0',
-  // // 016 - Damensalon
-  // 'ChIJp0sppLZPqEcRasG_5n9uw3A',
-  // // 017 - Fuks Bar
-  // 'ChIJIzn297ZPqEcRgw6RoWPK3GU',
-  // // 018 - Myxa Berlin
-  // 'ChIJn6jqirZPqEcRSTM-cJbVGAY',
-  // // 019 - Geist im Glas
-  // 'ChIJo99QRrZPqEcRXW-E2PeTJ4A',
-  // // 020 - Fuchsbau
-  // 'ChIJRSQRf8tPqEcRw5esshbdL8k',
+  // 031 - La Pecora Nera
+  'ChIJ17uJN75PqEcRKVO6yDVtNog',
+  // 032 - Sala da Mangiare
+  'ChIJvwxxdbxPqEcRimRthUrKjUs',
+  // 033 - LIMONADIER Cocktailbar
+  'ChIJoa2VQShQqEcRA7c9NHuooAA',
+  // 034 - Landwehrkanalufer
+  'ChIJo341is1PqEcRK8FKsc6xA0A',
+  // 035 - Kirk Bar
+  'ChIJ8Saj101OqEcRBXa22Ic61B4',
+  // 036 - Cuore Di Vetro
+  'ChIJVwi_HOJRqEcRdZxCHyZt1D0',
+  // 037 - Kosmetiksalon Babette
+  'ChIJEfO84BdOqEcRD1BMG5OkIvE',
+  // 038 - Sheriff Teddy Bar
+  'ChIJ1dfMe19OqEcRaAo1wYu7_nE',
+  // 039 - PLACE CLICHY
+  'ChIJteBFYVlOqEcRwPmMHbasUKU',
+  // 040 - WohnZimmer
+  'ChIJkdsxhf9NqEcROqrpU0E2Wdo',
 
-  // ------------------------------------------
-  // more than 20
+  // 041 - Park am Wasserturm
+  'ChIJheO_BwNOqEcRKANj6C1rPzY',
+  // 042 - Bitte und Danke
+  'ChIJp5s5Q2BOqEcRA7z8fxMJRbk',
+  // 043 - Atopia Kaffeehaus
+  'ChIJ48HH5P1NqEcRJonad7BUUEc',
+  // 044 - Absinthdepot Berlin
+  'ChIJL3N7C-FRqEcRlz-AbriEvhU',
+  // 045 - LOLA was here
+  'ChIJl5fmoQVSqEcROIldStvJvTw',
 
-  // // 021 - Pony Saloon Berlin
-  'ChIJqwCqdclPqEcRYUkH0bnBeFk'
+  // ------------
+  // 046 - LAST FOR NOW - Dujardin WEDDING
+  'ChIJ6yHZuChSqEcRPlv3ejkpuec'
 ]
 
 export default placeIdArray
