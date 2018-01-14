bcb11608e84b8cfe67569061b7afb86cfb10b2ba
Valerio Sillari <valerio.sillari@gmail.com>
2018-01-14 02:43:34 +0100

    [skip ci] set markers as custom icons

--- components/MapGoogle/MapGoogle.vue
+++ components/MapGoogle/MapGoogle.vue
@@ -84,13 +84,36 @@
         // right now more than 7 seconds ...
         const timerFallbackRound = timerFirstRound * placeItemsNumber / 2 + timerFirstRound
 
+        // marker custom colors
+        // custom color: a little bit darker then the main one. for the marker looks the same
+        const markerColorFill = '#eab622'
+        const markerColorStroke = '#FFC832'
+        // path inspiration from this codepen:
+        // https://codepen.io/defvayne23/pen/EVYGRw?editors=1010
+        // we follow the svg path to amazon and taken the path from the original url
+        // https://s3-us-west-2.amazonaws.com/s.cdpn.io/134893/pin-red.svg
+        const markerPath = 'M 8 2.1 c 1.1 0 2.2 0.5 3 1.3 c 0.8 0.9 1.3 1.9 1.3 3.1 s -0.5 2.5 -1.3 3.3 l -3 3.1 l -3 -3.1 c -0.8 -0.8 -1.3 -2 -1.3 -3.3 c 0 -1.2 0.4 -2.2 1.3 -3.1 c 0.8 -0.8 1.9 -1.3 3 -1.3 Z'
+
+        const customMarker = {
+          path: markerPath,
+          fillColor: markerColorFill,
+          strokeColor: markerColorStroke,
+          fillOpacity: 1,
+          scale: 2.5,
+          strokeWeight: 2,
+          // for correct alignmnet of custom SVG icon with map point
+          anchor: new google.maps.Point(11, 12)
+        }
+
         const markerOptions = (map, placeID, result) => {
           const marker = new google.maps.Marker({
             map: map,
             place: {
               placeId: placeID,
               location: result.geometry.location
-            }
+            },
+            // set icon custom style
+            icon: customMarker
           })
           return marker
         }
