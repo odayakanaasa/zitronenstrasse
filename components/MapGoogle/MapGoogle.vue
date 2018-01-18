d43f2897f287242ba8e12b19253fe83a07106f98
Valerio Sillari <valerio.sillari@gmail.com>
2018-01-18 18:32:43 +0100

    [skip ci] vue map: set custom icon shape and styles

--- components/MapGoogle/MapGoogle.vue
+++ components/MapGoogle/MapGoogle.vue
@@ -11,7 +11,7 @@
         :key='index'
         v-for='(marker, index) in markers'
         :position='marker.position'
-        :clickable='true'
+        :icon="mapIconShape"
       )
 </template>
 
@@ -64,6 +64,17 @@
   // import placeIdArray from '~/components/MapGoogle/_placesIdArrays.js'
   import mapStylesDark from '~/components/MapGoogle/_mapStylesDark.js'
 
+  // marker custom colors
+  // custom color: a little bit darker then the main one. for the marker looks the same
+  const markerColorFill = '#eab622'
+  const markerColorStroke = '#FFC832'
+
+  // path inspiration from this codepen:
+  // https://codepen.io/defvayne23/pen/EVYGRw?editors=1010
+  // we follow the svg path to amazon and taken the path from the original url
+  // https://s3-us-west-2.amazonaws.com/s.cdpn.io/134893/pin-red.svg
+  const markerPath = 'M 8 2.1 c 1.1 0 2.2 0.5 3 1.3 c 0.8 0.9 1.3 1.9 1.3 3.1 s -0.5 2.5 -1.3 3.3 l -3 3.1 l -3 -3.1 c -0.8 -0.8 -1.3 -2 -1.3 -3.3 c 0 -1.2 0.4 -2.2 1.3 -3.1 c 0.8 -0.8 1.9 -1.3 3 -1.3 Z'
+
   export default {
     data () {
       const MapGoogleLogic = {
@@ -102,7 +113,17 @@
               lng: 13.422217
             }
           }
-        ]
+        ],
+        mapIconShape: {
+          path: markerPath,
+          fillColor: markerColorFill,
+          strokeColor: markerColorStroke,
+          fillOpacity: 1,
+          scale: 2.5,
+          strokeWeight: 2
+          // for correct alignmnet of custom SVG icon with map point
+          // anchor: new google.maps.Point(11, 12)
+        }
       }
 
       return MapGoogleLogic
